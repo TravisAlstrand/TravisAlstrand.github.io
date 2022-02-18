393ffd09a3f19a1df4f85d694aa5566c9f0821d1
TravisAlstrand <travisalstrand@gmail.com>
2022-02-18 10:37:39 -0800

    Commit 4
    Project Modals functional and working.

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,45 @@
-const modalBtns = document.querySelectorAll('project-btn');
+const modalBtns = document.querySelectorAll('#project-btn');
 
-const modal1 = document.getElementById('project-modal-1');
-const modal2 = document.getElementById('project-modal-2');
-const modal3 = document.getElementById('project-modal-3');
-const modal4 = document.getElementById('project-modal-4');
+const modals = document.querySelectorAll('#project-modal');
 
-const modalCloseBtn = document.querySelector('.modal-close-btn');
+const modalCloseBtn = document.querySelectorAll('.modal-close-btn');
 
-// let modalOpen = false;
+modalBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const target = e.target;
 
-modalBtns.addEventListener('click', (e) => {
-    const target = e.target;
-
-    if (target.classList.contains('1')) {
-        modal1.style.display = "block";
-    }
+        if (target.classList.contains('1')) {
+            for(let i = 0; i < modals.length; i++) {
+                if (modals[i].classList.contains('pm1')) {
+                    modals[i].style.display = "block";
+                }
+            }
+        } else if (target.classList.contains('2')) {
+            for(let i = 0; i < modals.length; i++) {
+                if (modals[i].classList.contains('pm2')) {
+                    modals[i].style.display = "block";
+                }
+            }
+        } else if (target.classList.contains('3')) {
+            for(let i = 0; i < modals.length; i++) {
+                if (modals[i].classList.contains('pm3')) {
+                    modals[i].style.display = "block";
+                }
+            }
+        } else if (target.classList.contains('4')) {
+            for(let i = 0; i < modals.length; i++) {
+                if (modals[i].classList.contains('pm4')) {
+                    modals[i].style.display = "block";
+                }
+            }
+        }
+    })
 });
 
-
-console.log(modalBtns);
-
-// modal1Btn.addEventListener('click', () => {
-//     if (modalOpen === false) {
-//         modal1.style.display = "block";
-//         modalOpen = true;
-//     }
-// });
-
-
-
-// modalCloseBtn.addEventListener('click', () => {
-//     if (modalOpen === true) {
-//         modal1.style.display = "none";
-//         modalOpen = false;
-//     }
-// });
+modalCloseBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const target = e.target;
+        target.parentNode.parentNode.style.display = "none";
+    })
+});
 
